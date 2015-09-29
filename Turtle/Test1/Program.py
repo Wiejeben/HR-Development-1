ff4b0753e1051aaec5cf6cdfba22040d5afe1541
Maarten de Graaf <wiejeben@gmail.com>
2015-09-29 11:01:45 +0200

    Finish turtle assignment

---
+++ Turtle/Test1/Program.py
@@ -0,0 +1,32 @@
+﻿from Start import *
+
+# Perferences
+speed = 5
+turnDegrees = 30
+
+# Define ins
+print "Press these buttons to move the turtle:\n\rW = go forward\n\rS = reverse\n\rA = rotate left\n\rD = rotate right\n\r"
+
+def Program():
+    key = get()
+
+    #W go forward
+    if key == 119:
+        forward(speed)
+
+    #S reverse
+    if key == 115:
+        turn(180)
+        forward(speed)
+
+    #A rotate left
+    if key == 97:
+        turn(-turnDegrees)
+
+    #D rotate right
+    if key == 100:
+        turn(turnDegrees)
+
+
+run(Program)
+from End import *
