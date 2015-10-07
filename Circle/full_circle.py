682f0b236226a46a4059a1ef613ab85020271009
Maarten de Graaf <wiejeben@gmail.com>
2015-10-07 11:22:59 +0200

    Create circle program

--- Circle/full_circle.py
+++ Circle/full_circle.py
@@ -1,1 +1,25 @@
-__author__ = 'Maarten'
+import math
+
+diameter = input("Enter the diameter of the full circle: ")
+radius = diameter / 2
+result = ""
+
+for i in range(diameter):
+
+    # Calculate left half
+    row = (i - radius) * (i - radius)
+
+    for j in range(diameter):
+
+        # Calculate right half
+        column = (j - (radius)) * (j - (radius))
+
+        # If cursor position is within the circle
+        if math.sqrt(row + column) < radius:
+            result += "*"
+        else:
+            result += " "
+
+    result += "\n"
+
+print result
