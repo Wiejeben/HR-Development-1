2ad179d09958caf340d075b3457d81848a1a09b3
Maarten de Graaf <wiejeben@gmail.com>
2015-10-07 13:00:16 +0200

    Add comment

--- Circle/full_circle.py
+++ Circle/full_circle.py
@@ -8,7 +8,7 @@ for i in range(diameter):
 
     for j in range(diameter):
 
-        # If cursor position is within the circle
+        # If cursor position is within the circle via formula of Pythagoras
         if math.sqrt((i - radius) * (i - radius) + (j - (radius)) * (j - (radius))) < radius:
             result += "*"
         else:
