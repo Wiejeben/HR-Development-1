6797a36df15daaf64c300d38a54a3a8ebd45925f
Maarten de Graaf <wiejeben@gmail.com>
2015-10-07 13:40:51 +0200

    Applying more logical variables

--- Circle/full_circle.py
+++ Circle/full_circle.py
@@ -4,12 +4,12 @@ diameter = input("Enter the diameter of the full circle: ")
 radius = diameter / 2
 result = ""
 
-for i in range(diameter):
+for y in range(diameter):
 
-    for j in range(diameter):
+    for x in range(diameter):
 
         # If cursor position is within the circle via formula of Pythagoras
-        if sqrt(pow(i - radius, 2) + pow(j - radius, 2)) < radius:
+        if sqrt(pow(x - radius, 2) + pow(y - radius, 2)) < radius:
             result += "*"
         else:
             result += " "
