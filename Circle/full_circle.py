4d06a9dbeb6d7665040d239176d0caf20de76859
Maarten de Graaf <wiejeben@gmail.com>
2015-10-07 13:32:31 +0200

    Compact code

--- Circle/full_circle.py
+++ Circle/full_circle.py
@@ -9,7 +9,7 @@ for i in range(diameter):
     for j in range(diameter):
 
         # If cursor position is within the circle via formula of Pythagoras
-        if math.sqrt((i - radius) * (i - radius) + (j - (radius)) * (j - (radius))) < radius:
+        if math.sqrt((i - radius) * (i - radius) + (j - radius) * (j - radius)) < radius:
             result += "*"
         else:
             result += " "
