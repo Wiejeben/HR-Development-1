674edbcbaf7297cfa39f7ce0f4820b0e8900cb18
Maarten de Graaf <wiejeben@gmail.com>
2015-10-07 12:59:37 +0200

    Compact code

--- Circle/full_circle.py
+++ Circle/full_circle.py
@@ -6,16 +6,10 @@ result = ""
 
 for i in range(diameter):
 
-    # Calculate left half
-    row = (i - radius) * (i - radius)
-
     for j in range(diameter):
 
-        # Calculate right half
-        column = (j - (radius)) * (j - (radius))
-
         # If cursor position is within the circle
-        if math.sqrt(row + column) < radius:
+        if math.sqrt((i - radius) * (i - radius) + (j - (radius)) * (j - (radius))) < radius:
             result += "*"
         else:
             result += " "
