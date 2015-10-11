2ff22c9b9778de9b473d58dfa63f731414b2687c
Maarten de Graaf <wiejeben@gmail.com>
2015-10-11 12:27:04 +0200

    Optimise RPS

--- RPS/b.py
+++ RPS/b.py
@@ -1,17 +1,3 @@
-import random
-
-# -- Notes
-# Scissors cut Paper
-# Paper covers Rock
-# Rock crushes Lizard
-# Lizard poisons Spock
-# Spock smashes Scissors
-# Scissors decapitate Lizard
-# Lizard eats Paper
-# Paper disproves Spock
-# Spock vaporizes Rock
-# Rock crushes Scissors
-
 # Options
 options = [
     "rock",
